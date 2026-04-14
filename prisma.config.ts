53db8f28b787a9c993a26fc3ae4147d3d89fb356
Methasit-Pun <methasitpun@gmail.com>
2026-04-14 15:56:03 +0700

    fix: cicd lint and typecheck issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "prisma/config"
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
   datasource: {
-    url: process.env.DIRECT_URL,
+    url: process.env.DIRECT_URL ?? "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 })
